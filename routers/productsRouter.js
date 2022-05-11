10ba9612a2bc1fd777901e2285421c99f7a8c83b
Rafael <rafaelongo45@gmail.com>
2022-05-11 02:45:49 -0300

    Now the user can get all the products in the database, select specific products by genre or get a specific product by its id

--- routers/productsRouter.js
+++ routers/productsRouter.js
@@ -1,10 +1,14 @@
 import { Router } from 'express';
 
+import { validateId } from '../middlewares/validateId.js';
 import { validateProduct } from "../schemas/productSchema.js";
-import { addProduct } from '../controllers/productsController.js';
+import { addProduct, getProducts, getSpecificProduct } from '../controllers/productsController.js';
 
 const productsRouter = Router();
 
+productsRouter.get('/products', getProducts);
+productsRouter.get('/products/:id', validateId, getSpecificProduct);
 productsRouter.post('/products', validateProduct, addProduct);
+
 
 export default productsRouter;
