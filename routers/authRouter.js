d1ef0670904ca68d785def2f8ef7e11025364ef4
simone-alcala <simone.alcala@gmail.com>
2022-05-12 19:20:24 -0300

    Deleted schema validation to user address on signUp

--- routers/authRouter.js
+++ routers/authRouter.js
@@ -3,11 +3,11 @@ import { Router } from 'express';
 import { validateSignIn } from '../middlewares/validateSignIn.js';
 import { validateSignUp } from './../middlewares/validateSignUp.js';
 import { signUp, signIn } from './../controllers/authController.js';
-import { validateUser, schemaSignIn } from './../schemas/authSchema.js';
+import { schemaSignUp, schemaSignIn } from './../schemas/authSchema.js';
 
 const authRouter = Router();
 
-authRouter.post('/signup', validateUser, validateSignUp, signUp );
+authRouter.post('/signup', schemaSignUp, validateSignUp, signUp );
 authRouter.post('/signin', schemaSignIn, validateSignIn, signIn );
 
 export default authRouter;
--- schemas/authSchema.js
+++ schemas/authSchema.js
@@ -7,31 +7,37 @@ const options = {
   convert     : false
 }
 
-export function validateUser (req,res,next) {
+export function schemaSignUp (req,res,next) {
 
   const newUser = req.body;   
+
+  let checkAvatar = true;
+  if (newUser.avatar && newUser.avatar !== '') {
+    const regex = new RegExp(/^http[^\?]*.(jpg|jpeg|gif|png|tiff|bmp)(\?(.*))?$/);
+    checkAvatar = regex.test(newUser.avatar);
+  }
 
   const schema    = joi.object({
     name:           joi.string().trim().required(),
     email:          joi.string().trim().email().required(),
-    avatar:         joi.string().pattern(/^http[^\?]*.(jpg|jpeg|gif|png|tiff|bmp)(\?(.*))?$/),
-    cpf:            joi.string().length(14).trim().required(),
-    address:        joi.string().trim().required(),
-    addressNumber:  joi.string().trim().required(),
-    complement:     joi.string().trim(),
-    neighborhood:   joi.string().trim().required(),
-    zipCode:        joi.string().length(9).trim().required(),
-    city:           joi.string().trim().required(),
-    state:          joi.string().length(2).trim().required(),//TODO: add states as only possible values (RJ, SP, MG...)
     password:       joi.string().required(),
     repeat_password: joi.ref('password')
   }) .with('password', 'repeat_password');;
 
   const validate = schema.validate(newUser,options);
-  
-  if (validate.error) 
-    return res.status(422).send(validate.error.details.map(detail => detail.message));
 
+  let errorMessages = [];
+  
+  if (validate.error) {
+    errorMessages = validate.error.details.map(detail => detail.message);
+  }  
+  
+  if (!checkAvatar) errorMessages.push('Invalid url avatar');
+
+  if (errorMessages.length > 0){
+    return res.status(422).send(errorMessages);
+  }
+  
   next();
 }
 
